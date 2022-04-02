4d037a402c4cc7f261b7cbba8610c7fb12827546
qureshi <ishrat.qureshi@pwc.com>
2022-04-02 23:19:14 +0500

    Api - integrated and flag css correction

---
+++ script.js
@@ -0,0 +1,11 @@
+const app = () => {
+	// window.addEventListener('load', (event) => {
+	// 	console.log('page is fully loaded');
+	// });
+	const body = document.getElementById('body-id');
+	body.addEventListener('load', () => {
+		console.log('ishrt');
+	});
+};
+
+app();
